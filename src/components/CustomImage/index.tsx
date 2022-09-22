3183843c5dff03c39cc1ada29ad0cee736e06814
ZiiM <alextede8899@gmail.com>
2022-09-22 01:21:45 -0400

    Start the layout add some basic components.

---
+++ src/components/CustomImage/index.tsx
@@ -0,0 +1,54 @@
+import classNames from 'classnames';
+import { ImageProps as NextImageProps } from 'next/image';
+import React from 'react';
+
+type ImageProps = Partial<
+  Omit<NextImageProps, 'width' | 'height' | 'layout'>
+> & {
+  src?: string;
+  name: string;
+  className?: string;
+} & (
+    | { width: number | string; height: number | string }
+    | { layout: 'fixed' | 'fill' | 'intrinsic' | 'responsive' | undefined }
+  );
+
+const CustomImage: React.FC<ImageProps> = ({
+  name,
+  src,
+  className,
+  ...rest
+}) => {
+  return src ? (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src={src}
+      alt={name}
+      className={classNames(
+        'w-[44px] h-[44px] items-center justify-center select-none cursor-pointer text-center flex',
+        className
+      )}
+      {...rest}
+    />
+  ) : (
+    <div
+      className={classNames(
+        'w-[44px] h-[44px] items-center justify-center select-none cursor-pointer text-center flex',
+        className
+      )}
+      {...rest}
+    >
+      {getInitials(name)}
+    </div>
+  );
+};
+
+export default CustomImage;
+
+const getInitials = (name: string) => {
+  const [first, last] = name.split(' ');
+  return first && last
+    ? `${first.charAt(0)}${last.charAt(0)}`
+    : first?.charAt(0);
+};
+
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import CustomImage from '../CustomImage';
 
 const Navbar: React.FC = () => {
   return (
     <nav className='flex flex-row w-full max-w-xs navbar'>
       <div className='h-screen p-2 w-full bg-main-black-800 max-w-[4rem] items-center flex flex-col'>
-        <button className='p-2 rounded-full bg-main-black-500 w-[44px] h-[44px]'>
+        <button className='rounded-full bg-main-black-500 p-2 flex w-[44px] h-[44px] items-center justify-center'>
           Test
         </button>
         <hr className='w-full mt-2 bg-main-black-500 text-main-black-500 border-main-black-500' />
-        <div className='flex flex-col mt-2 space-y-2'></div>
+        <div className='flex flex-col mt-2 space-y-2'>
+          <CustomImage
+            name={'Test Test'}
+            width={'44px'}
+            height={'44px'}
+            className={'rounded-full bg-main-black-500 font-bold text-lg'}
+          />{' '}
+          <CustomImage
+            name={'Test Test'}
+            src={
+              'https://images.unsplash.com/photo-1660216140102-c25afd109129?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=699&q=80'
+            }
+            layout={'fill'}
+            className={'rounded-full bg-main-black-500 font-bold text-lg'}
+          />
+        </div>
+        <div></div>
+      </div>
+      <div className={'h-screen w-60 bg-main-black-700 p-2'}>
+        <button className='w-full p-2 bg-main-black-400 rounded-md text-start'>
+          Test
+        </button>
       </div>
     </nav>
   );
