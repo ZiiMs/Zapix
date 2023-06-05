a5d629cec9c4f908dcaa6585e38eb0b0f0fe3125
ZiiM <alextede8899@gmail.com>
2023-06-04 20:51:26 -0400

    Fix bug with createdAt date in messages.

--- apps/nextjs/src/components/Message/index.tsx
+++ apps/nextjs/src/components/Message/index.tsx
@@ -1,6 +1,7 @@
-import classNames from 'classnames';
-import React from 'react';
-import CustomImage from '../CustomImage';
+import React, { useEffect } from "react";
+import classNames from "classnames";
+
+import CustomImage from "../CustomImage";
 
 const Message: React.FC<{
   username: string;
@@ -12,24 +13,26 @@ const Message: React.FC<{
   return (
     <div
       className={classNames(
-        'flex flex-row items-start justify-start w-full h-fit py-2',
-        classNamesProp
+        "flex h-fit w-full flex-row items-start justify-start py-2",
+        classNamesProp,
       )}
     >
-      <div className='px-2'>
+      <div className="px-2">
         <CustomImage
           name={username}
           src={image}
           width={36}
           height={36}
-          className={'rounded-full bg-rad-black-500 font-bold text-lg'}
+          className={"rounded-full bg-rad-black-500 text-lg font-bold"}
         />
       </div>
-      <div className='flex flex-col'>
-        <div className='flex flex-row gap-x-2 items-start text-start justify-start'>
-          <span className='font-bold '>{username}</span>
-          <span className='italic text-rad-light-400/30 text-sm'>
-            {createdAt.toLocaleDateString()}
+      <div className="flex flex-col">
+        <div className="flex flex-row items-start justify-start gap-x-2 text-start">
+          <span className="font-bold ">{username}</span>
+          <span className="text-sm italic text-rad-light-400/30">
+            {typeof createdAt === "string"
+              ? new Date(createdAt).toLocaleDateString()
+              : createdAt.toLocaleDateString()}
           </span>
         </div>
         <span>{text}</span>
@@ -39,4 +42,3 @@ const Message: React.FC<{
 };
 
 export default Message;
-
